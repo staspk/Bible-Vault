cc32ef0935a3b203419e1092de63c61602b798fa
Stanislav Kozubenko <staspk@gmail.com>
2025-03-15 22:33:23 -0700

    comments added

--- python/definitions.py
+++ python/definitions.py
@@ -1,12 +1,14 @@
 import os
 
 
-DEFINITIONS_PY_ABSOLUTE_PATH  =  os.path.abspath(__file__)
+DEFINITIONS_PY                =  os.path.abspath(__file__)                              # absolute path:  ...\Bible-Vault\python\definitions.py
 
-PYTHON_ROOT_DIRECTORY         =  os.path.dirname(DEFINITIONS_PY_ABSOLUTE_PATH)      # os.path.dirname(abs_path) => Go up one level
-PROJECT_ROOT_DIRECTORY        =  os.path.dirname(PYTHON_ROOT_DIRECTORY)
+PYTHON_ROOT_DIRECTORY         =  os.path.dirname(DEFINITIONS_PY)                        # absolute path:  ...\Bible-Vault\python
+PROJECT_ROOT_DIRECTORY        =  os.path.dirname(PYTHON_ROOT_DIRECTORY)                 # absolute path:  ...\Bible-Vault
 
+TEMP_DIRECTORY                =  os.path.join(PROJECT_ROOT_DIRECTORY, 'temp')
 JSONS_DIRECTORY               =  os.path.join(PROJECT_ROOT_DIRECTORY, 'jsons')
+
 BIBLE_JSON                    =  os.path.join(JSONS_DIRECTORY, 'bible.json')
 
 TEMPORARY_HTML                =  os.path.join(PYTHON_ROOT_DIRECTORY, 'temporary.html')
