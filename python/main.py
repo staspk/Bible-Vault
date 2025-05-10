6fe69b97ccbbb091dae11e41916602aa2b3e0d3d
Stanislav Kozubenko <staspk@gmail.com>
2025-05-10 06:24:01 -0700

    replaced with class enum pattern

--- python/main.py
+++ python/main.py
@@ -1,8 +1,7 @@
 from definitions import *
-from html_parser import parse_simple_html
-from scrape_bible import *
-from models.Bible import *
-
+from models.Bible import BIBLE
+from html_parser import *
+from scrape import *
 
 
 eng_translations = ['KJV', 'NKJV', 'NASB', 'ESV', 'RSV', 'NRSV', 'NIV', 'NET']
@@ -11,5 +10,7 @@ russ_translations = ['RUSV', 'NRT']
 
 # stealth_scrape_chapter(Bible[BIBLE.GENESIS], 1, russ_translations)
 
-# stealth_scrape(Bible[BIBLE.GENESIS], 'KJV')
-parse_simple_html(Bible[BIBLE.GENESIS], 'RSV', one_pass=True)
+
+stealth_scrape(BIBLE.GENESIS, 'KJV')
+parse_simple_html(BIBLE.GENESIS, 'KJV', one_pass=False)
+
