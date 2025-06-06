8f7bc3d5b864ec32941c8a9363c04b0a76dbda00
Stanislav Kozubenko <staspk@gmail.com>
2025-06-06 15:43:20 -0700

    load_into_memory() stub

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 from definitions import *
+from kozubenko.os import Directory, Parent, TestPath
 from models.Bible import BIBLE
 from html_parser import *
 from scrape import *
@@ -8,9 +9,33 @@ eng_translations = ['KJV', 'NKJV', 'NASB', 'ESV', 'RSV', 'NRSV', 'NIV', 'NET']
 russ_translations = ['RUSV', 'NRT']
 
 
-# stealth_scrape_chapter(Bible[BIBLE.GENESIS], 1, russ_translations)
+# stealth_scrape_chapter(BIBLE.GENESIS, 1, russ_translations)
+
+scrape_chapter(BIBLE.DANIEL, 2, 'NASB')
+
+# stealth_scrape_one(BIBLE.DANIEL, eng_translations, 1)
+# parse_html(BIBLE.DANIEL, 'NASB', True)
 
 
-stealth_scrape(BIBLE.GENESIS, 'KJV')
-parse_simple_html(BIBLE.GENESIS, 'KJV', one_pass=False)
 
+
+
+
+
+
+
+
+
+
+
+
+# def load_into_memory(path:str):
+#     if not os.path.exists(path):
+#         raise Exception(f'path does not exist: {path}')
+    
+#     for book in BIBLE.book_list():
+#        print_cyan(book.name)
+
+# main = Parent(Parent(__file__)) + r'\bible_txt\\RSV-v1'
+
+# load_into_memory(main)
