a6d702f58fe716ee22f3cff231f0272f153ed95b
Stanislav Kozubenko <staspk@gmail.com>
2025-06-19 12:57:50 -0700

    testing new functions

--- node/app.ts
+++ node/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import { DEFINITIONS_TS } from "./definitions.js";
+import { assert_string } from "./kozubenko/utils";
+import { print } from "./kozubenko/print.js";
+
+console.log("DEFINITIONS_TS:", DEFINITIONS_TS);
+print("DEFINITIONS_TS", DEFINITIONS_TS);
+
+const aString = "hello";
+assert_string("aString", aString)
