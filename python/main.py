9eeac956c519e86f627e7f76f89c010ddbb99483
Stanislav Kozubenko <staspk@gmail.com>
2025-06-19 12:59:36 -0700

    skipping psalms for now...

--- python/main.py
+++ python/main.py
@@ -5,9 +5,12 @@ from models.Bible import BIBLE
 from html_parser import *
 from scrape import *
 
+"""
+Skip Psalms for now...
+"""
 
 eng_translations = ['KJV', 'NKJV', 'NASB', 'ESV', 'RSV', 'NRSV', 'NIV', 'NET']
 russ_translations = ['RUSV', 'NRT']
 
-scrape_bible_txt(['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'], 18)
+scrape_bible_txt(['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'], 21)
 # scrape_bible_txt(['NIV', 'NET', 'ESV', 'NLT', 'CEV'])
