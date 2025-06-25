70af65372849bb38993a76bfdc851b56674c174e
Stanislav Kozubenko <staspk@gmail.com>
2025-06-25 00:16:37 -0700

    +ScriptArg_1()

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 from definitions import *
 from kozubenko.os import Directory, Parent, TestPath
-from kozubenko.utils import assert_path_exists
+from kozubenko.utils import ScriptArg_1, assert_path_exists
 from models.Bible import BIBLE
 from html_parser import *
 from scrape import *
@@ -10,8 +10,10 @@ Skip Psalms for now...
 Issues also in Song of Solomon
 """
 
+start_scrape_index = int(ScriptArg_1(22))
+
 eng_translations = ['KJV', 'NKJV', 'NASB', 'ESV', 'RSV', 'NRSV', 'NIV', 'NET']
 russ_translations = ['RUSV', 'NRT']
 
-scrape_bible_txt(['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'], 21)
+scrape_bible_txt(['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'], start_scrape_index)
 # scrape_bible_txt(['NIV', 'NET', 'ESV', 'NLT', 'CEV'])
