366212eaebfe6f08b0a8bb43a07d3bfb42efaf87
Stanislav Kozubenko <staspk@gmail.com>
2025-06-24 22:59:28 -0700

    startup script for windows

--- start-server.ps1
+++ start-server.ps1
@@ -2,8 +2,13 @@ $location = $PWD.Path
 
 try {
     Set-Location ./node
-    npx tsx app.ts start-server.ps1
+    npx tsx app.ts
+    $exitCode = $LASTEXITCODE
 }
 finally {
     Set-Location $location
+
+    if($exitCode -eq 0) {
+        Clear-Host
+    }
 }
