e29fd191df797b3822364a20c8a08ca1543b9ba3
Stanislav Kozubenko <staspk@gmail.com>
2025-06-25 16:00:19 -0700

    Version 1.0

--- node/kozubenko/typing.ts
+++ node/kozubenko/typing.ts
@@ -23,16 +23,16 @@ function type(value) {
 * Example Use: `const aString = "hello"; assert_string("aString", aString)`
 */
 export function assert_string(varName: string, value: any): true | Error {  
-    if (typeof value !== "string") {
+    if (typeof value !== "string")
         throw new Error(`${varName} must be a string, but is: ${typeof value}`);
-    }
+
     return true;
 }
 
 /**
 * Use to enforce type at runtime. Either returns true, or throws Error (if not 'number' or 'bigint')
 * 
-* Example Use: `const aNumber = 5; assert_int("aNumber", aNumber)`
+* Example Use: `const aNum = 5; assert_int("aNum", aNum)`
 */
 export function assert_number(varName: string, value: any, minVal?: number, maxVal?: number): true | Error {  
     if (typeof value !== "number" && typeof value !== "bigint")
@@ -40,7 +40,6 @@ export function assert_number(varName: string, value: any, minVal?: number, maxV
 
     if (minVal !== undefined && value < minVal)
         throw new Error(`assert_number(${varName}): value less than minVal. minVal: ${minVal}. value: ${value}`);
-
     if (maxVal !== undefined && value > maxVal)
         throw new Error(`assert_number(${varName}): value greater than maxVal. maxVal: ${maxVal}. value: ${value}`);
 
@@ -48,17 +47,19 @@ export function assert_number(varName: string, value: any, minVal?: number, maxV
 }
 
 /**
-* Use to enforce type at runtime. List refers to string[]. Either returns true, or throws Error
+* Use to enforce type at runtime. List refers to any[]. Either returns true, or throws Error
 * 
 * Example Use: `const aList = ["hello", "goodbye"]; assert_list("aList", aList)`
 */
-export function assert_list(varName: string, value: any, minLen?:number, maxLen?:number): true | Error {  
-    if (!Array.isArray(value))
-        throw new Error(`${varName} must be a string[], but is: ${typeof value}`);
-    if (minLen && value.length < minLen)
-        throw new Error(`assert_list(${varName}): length of list < minLen. minLen: ${minLen}. actual length: ${value.length}`);
-    if (maxLen && value.length > maxLen)
-        throw new Error(`assert_list(${varName}): length of list > maxLen. maxLen: ${maxLen}. actual length: ${value.length}`);
+export function assert_list(varName: string, list: any, minLen?:number, maxLen?:number): true | Error {  
+    if (!Array.isArray(list))
+        throw new Error(`${varName} must be a string[], but is: ${typeof list}`);
+
+    if (minLen !== undefined && list.length < minLen)
+        throw new Error(`assert_list(${varName}): length of list < minLen. minLen: ${minLen}. actual length: ${list.length}`);
+    if (maxLen !== undefined && list.length > maxLen)
+        throw new Error(`assert_list(${varName}): length of list > maxLen. maxLen: ${maxLen}. actual length: ${list.length}`);
+
     return true;
 }
 
@@ -68,8 +69,8 @@ export function assert_list(varName: string, value: any, minLen?:number, maxLen?
 * Example Use: `const book = new Book('Revelation', 'Rev', 22, 66); assert_class("book", book, Book)`
 */
 export function assert_class(varName: string, object: any, _class: any): true | Error {  
-    if (!(object instanceof _class)) {
+    if (!(object instanceof _class)) 
         throw new Error(`${varName} must be a ${_class.name}, but is: ${object.constructor.name}`);
-    }
+
     return true;
 }
