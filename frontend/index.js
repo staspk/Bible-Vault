258552fd2ff1c926d831edd5dd815155cdef3e23
Stanislav Kozubenko <staspk@gmail.com>
2025-06-27 02:30:43 -0700

    search-input populates with url params

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,17 @@
-console.log('Are we there yet?');
+const searchInput = document.querySelector('.search-input');
+
+const urlParams = new URLSearchParams(window.location.search);
+const book         = urlParams.get('book');
+const chapter      = urlParams.get('chapter');
+const translations = (urlParams.get('translations') ?? '').split(';').filter(translation => translation);
+
+if (book)
+    searchInput.value = book;
+
+if (chapter)
+    searchInput.value += ` ${chapter}`;
 
 
+for (translation in translations) {
+    
+}
