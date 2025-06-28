3a9eb8a87cd66a14455f97cd859e25ff8c8253a4
Stanislav Kozubenko <staspk@gmail.com>
2025-06-28 00:35:24 -0700

    +handleResourceRequest() now handles static file requests

--- node/http-server.ts
+++ node/http-server.ts
@@ -11,14 +11,38 @@ print()
 const PORT = 8080;
 
 const INDEX_HTML = path.join(__dirname, 'frontend', 'index.html');
-const INDEX_JS   = path.join(__dirname, 'frontend', 'index.js');
-const INDEX_CSS  = path.join(__dirname, 'frontend', 'index.css');
+const PATHS      = {
+    '/Timer.js' : path.join(__dirname, 'frontend', 'Timer.js'),
+    '/index.js' : path.join(__dirname, 'frontend', 'index.js'),
+    '/index.css': path.join(__dirname, 'frontend', 'index.css'),
+};
+
+
+function handleResourceRequest(pathname:string, response: http.ServerResponse): void {
+    const path = PATHS[pathname];
+    if (!path) return;
+
+    fs.readFile(path, (error, data) => {
+        if (error) {
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            response.end(`Not Found: ${pathname.substring}`);
+            return;
+        }
+        
+        if (pathname.split('.')[1].toLowerCase() === 'html')
+            response.writeHead(200, {'Content-Type': 'text/html'});
+        if (pathname.split('.')[1].toLowerCase() === 'js')
+            response.writeHead(200, { 'Content-Type': 'application/javascript' });
+        if (pathname.split('.')[1].toLowerCase() === 'css')
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+
+        response.end(data);
+    });
+}
 
 const server = http.createServer((request, response) => {
-    if (!request.url) {
-        return
-    }
-
+    if (!request.url) return;
+    
     const urlObj = new URL(request.url, `http://localhost:${PORT}`);
     if (urlObj.pathname === '/') {                                               // GET /?book=Matthew&chapter=22&translation=NKJV;ESV 
         fs.readFile(INDEX_HTML, (error, data) => {
@@ -27,51 +51,31 @@ const server = http.createServer((request, response) => {
                 response.end('Error Loading: index.html');
                 return;
             }
-
             response.writeHead(200, {'Content-Type': 'text/html'});
-            response.end(data);
-        });
-    }
-    if (urlObj.pathname === '/index.js') {
-        fs.readFile(INDEX_JS, (error, data) => {
-            if (error) {
-                response.writeHead(404, { 'Content-Type': 'text/plain' });
-                response.end('Not Found: index.js');
-                return;
-            }
-            response.writeHead(200, { 'Content-Type': 'application/javascript' });
-            response.end(data);
-        });
-    }
-    if (urlObj.pathname === '/index.css') {
-        fs.readFile(INDEX_CSS, (error, data) => {
-            if (error) {
-                response.writeHead(404, { 'Content-Type': 'text/plain' });
-                response.end('Not Found: index.css');
-                return;
-            }
-            response.writeHead(200, { 'Content-Type': 'text/css' });
             response.end(data);
         });
     }
 
     if (urlObj.pathname === '/api/') {
         printYellow(`API Request: ${urlObj.pathname}`);
-
+        
         const param1: string = urlObj.searchParams.get('book') ?? '';
         const param2: string = urlObj.searchParams.get('chapter') ?? '';
         const param3: string = urlObj.searchParams.get('translations') ?? '';
-
+        
         const book = BIBLE.getBook(param1);
         const chapter = parseInt(param2, 10);
         const translations = param3.split(';').filter(translation => translation);
-
+        
         if (!book || !chapter || translations.length < 0) {
             response.writeHead(400, { 'Content-Type': 'application/json' });
             response.end(JSON.stringify({ error: 'Missing required query params' }));
             return;
         }
     }
+
+    else
+        handleResourceRequest(urlObj.pathname, response);
 });
 
 
