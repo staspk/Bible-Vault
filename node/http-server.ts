77874056d30046ac247703ef3b516f577a222746
Stanislav Kozubenko <staspk@gmail.com>
2025-07-08 15:29:10 -0700

    /api now returns translations

--- node/http-server.ts
+++ node/http-server.ts
@@ -1,12 +1,12 @@
 import * as http from 'http';
 import * as fs from 'fs';
-import * as path from 'path'
+import * as Path from 'path'
 import { fileURLToPath } from 'url';
 
 import { print, printGreen, printRed, printYellow } from './kozubenko/print.js';
-import { respondBadRequest } from './kozubenko/http.js';
+import { handleNotFound, respondBadRequest, Status } from './kozubenko/http.js';
 import { combinePaths } from './kozubenko/utils.js';
-import { BIBLE } from './models/Bible.js';
+import { BIBLE, Book } from './models/Bible.js';
 
 const __dirname = import.meta.dirname
 print('process.argv', process.argv);
@@ -15,9 +15,9 @@ print()
 
 const PORT = 8080;
 
-const INDEX_HTML = path.join(__dirname, 'vite-frontend', 'dist', 'index.html');
-const DIST       = path.join(__dirname, 'vite-frontend', 'dist');
-const BIBLE_TXT  = path.join(__dirname, '..', 'bible_txt');
+const INDEX_HTML = Path.join(__dirname, 'vite-frontend', 'dist', 'index.html');
+const DIST       = Path.join(__dirname, 'vite-frontend', 'dist');
+const BIBLE_TXT  = Path.join(__dirname, '..', 'bible_txt');
 
 function handleResourceRequest(pathname:string, response:http.ServerResponse): void {
     const requestedResource = combinePaths(DIST, pathname);
@@ -26,7 +26,7 @@ function handleResourceRequest(pathname:string, response:http.ServerResponse): v
         response.end();
         return;
     }
-
+    
     fs.readFile(requestedResource, (error, data) => {
         if (error) {
             response.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -36,15 +36,46 @@ function handleResourceRequest(pathname:string, response:http.ServerResponse): v
         
         if (pathname.split('.')[1].toLowerCase() === 'html')
             response.writeHead(200, {'Content-Type': 'text/html'});
-
+        
         if (pathname.split('.')[1].toLowerCase() === 'js')
             response.writeHead(200, { 'Content-Type': 'application/javascript' });
-
+        
         if (pathname.split('.')[1].toLowerCase() === 'css')
             response.writeHead(200, { 'Content-Type': 'text/css' });
-
+        
         response.end(data);
     });
+}
+
+/**
+ * @returns 
+ */
+async function loadChapterIntoMemory(path:string): Promise<object|null> { 
+    try {
+        const data = await fs.promises.readFile(path, 'utf-8');
+        const lines = data.split(/\r?\n/);
+        
+        // Use 1-based indexing
+        return Object.fromEntries(
+            lines.map((line, i) => [ (i + 1).toString(), line ])
+        );
+    } catch (error) {
+        printRed(`readChapter(): ${error}`);
+        return null;
+    }
+}
+
+/**
+* Tests using the data: array object's value of the 1'st property
+* data { 'nkjv': null, 'rsv': `${data}` }
+*
+*/
+function checkStatus(data): Status {
+    
+    if(data.every(obj => Object.values(obj)[0] !== null))
+        return Status.Success;
+        return Status.Error;
+    return Status.Partial;
 }
 
 const server = http.createServer((request, response) => {
@@ -71,26 +102,46 @@ const server = http.createServer((request, response) => {
         const param1:string = urlObj.searchParams.get('book') ?? '';
         const param2:string = urlObj.searchParams.get('chapter') ?? '';
         const param3:string = urlObj.searchParams.get('translations') ?? 'KJV,NKJV,RSV,NRSV,NASB';
-
+        
         if (!param1 || !param2) {  respondBadRequest(response); return;  }
         
-        const book = BIBLE.getBook(param1);
-        const chapter = parseInt(param2, 10);
-        const translations = param3 ? param3.split(',').filter(translation => translation) : ['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'];
+        const book = BIBLE.getBook(param1);     // Stan: yes, you handle garbage input ahead with a falsy check
+        const chapter = parseInt(param2, 10);   // Stan: yes, you handle garbage input ahead with a falsy check
+        const translations: string[] = param3 ? param3.split(',').filter(translation => translation) : ['KJV', 'NKJV', 'RSV', 'NRSV', 'NASB'];  // Stan: garbage rejected on testpath
         
         if (!book || !chapter || translations.length < 1) {  respondBadRequest(response); return;  }
+        
+        if (chapter < 1 || chapter > book.chapters) {  respondBadRequest(response, `${book.name}:${chapter} is not a real Bible chapter.`); return;  }
+        
+        (async () => {
+            const promises = translations.map(async translation => {
+                const chapterFile = Path.join(BIBLE_TXT, translation, book.name, `${chapter}.txt`);
+                if (fs.existsSync(chapterFile))
+                    // return await loadChapterIntoMemory(chapterFile);
+                    return { [translation.toUpperCase()]: await loadChapterIntoMemory(chapterFile) };
+                
+                return { [translation.toUpperCase()]: null };
+            });
+            
+            const data = await Promise.all(promises);
 
-        if (chapter < 1 || chapter > book.chapters) {  respondBadRequest(response, `${book.name}:${chapter} does not exist.`); return;  }
-
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify({
-            book,
-            chapter,
-            translations,
-            message: 'Data retrieved successfully.'
-        }));
-
-        printGreen(`Response sent: ${book.name}:${chapter} - ${translations}`)
+            if (data.every(obj => Object.values(obj)[0] !== null)) {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify({
+                    status: Status.Success,
+                    data: data,
+                }));
+                return;
+            } else if (data.every(obj => Object.values(obj)[0] === null)) {
+                handleNotFound(response); return;
+            } else {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify({
+                    status: Status.Success,
+                    data: Object.assign({}, ...data)
+                }));
+            }
+        })();
     }
 
     else
--- node/kozubenko/http.ts
+++ node/kozubenko/http.ts
@@ -1,14 +1,27 @@
-import * as http from 'http'
+import { ServerResponse } from 'http';
 
 /**
- * 400 - Bad Request `{ 'Content-Type': 'application/json' }`
- *
- * This function is intended to be used when the client provides
- * malformed or invalid request parameters.
- *
- * @returns {void}
- */
-export function respondBadRequest(response:http.ServerResponse, errorMsg = 'Malformed/Invalid Required Query Params'): void {
+* 400 - Bad Request `{ 'Content-Type': 'application/json' }`
+*
+* This function is intended to be used when the client provides
+* malformed or invalid request parameters.
+*
+* @returns {void}
+*/
+export function respondBadRequest(response:ServerResponse, errorMsg = 'Malformed/Invalid Required Query Params'): void {
     response.writeHead(400, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ error: errorMsg }));
+    response.end(JSON.stringify({ ok: false, error: errorMsg }));
+}
+
+
+export function handleNotFound(response:ServerResponse, errorMsg = 'Requested resource(s) '): void {
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ ok: false, error: errorMsg }));
+}
+
+
+export enum Status {
+    Success = "success",
+    Partial = "partial",
+    Error   = "error"
 }
