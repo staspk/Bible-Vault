b1ba3aafbb8248ec4f8fe75c123925b17dfdd533
Stanislav Kozubenko <staspk@gmail.com>
2025-07-09 02:46:15 -0700

    docstrings re-written for http response helpers

--- node/kozubenko/http.ts
+++ node/kozubenko/http.ts
@@ -3,17 +3,18 @@ import { ServerResponse } from 'http';
 /**
 * 400 - Bad Request `{ 'Content-Type': 'application/json' }`
 *
-* This function is intended to be used when the client provides
-* malformed or invalid request parameters.
-*
-* @returns {void}
+* Intended to be used when the client provides malformed or invalid request parameters.
 */
 export function respondBadRequest(response:ServerResponse, errorMsg = 'Malformed/Invalid Required Query Params'): void {
     response.writeHead(400, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify({ ok: false, error: errorMsg }));
 }
 
-
+/**
+* 404 - Not Found `{ 'Content-Type': 'application/json' }`
+*
+* Intended to be used when the requested resource(s) could not be found.
+*/
 export function handleNotFound(response:ServerResponse, errorMsg = 'Requested resource(s) '): void {
     response.writeHead(404, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify({ ok: false, error: errorMsg }));
