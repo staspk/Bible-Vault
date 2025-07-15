c0c47acf7f8c487bc2e9fa66da65d5b4f3516a09
Stanislav Kozubenko <staspk@gmail.com>
2025-07-15 03:11:16 -0700

    file deleted

--- node/definitions.ts
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const DEFINITIONS_TS = resolve(__filename);
-
-export { DEFINITIONS_TS };
