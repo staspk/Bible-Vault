b7bad0fcf9b42d4c8d2f29984aaa02e8bfd0ba93
Stanislav Kozubenko <staspk@gmail.com>
2025-07-20 09:15:49 +0000

    template change

--- node/_vite-frontend/src/components/PassageDiv.ts
+++ node/_vite-frontend/src/components/PassageDiv.ts
@@ -1,7 +1,7 @@
 import { BIBLE, Book } from "../../../_shared/Bible";
 import type { IChapterResponse } from "../../../_shared/interfaces"
 
-
+/** CSS class that's used to determine `width` and `grid-template-columns` for  `#passage-div` */
 const COLUMNS_CLASS = (num_of_cols) => `cols-${num_of_cols}`;
 
 /**  #passage-div  */
