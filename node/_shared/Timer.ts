1f55272e4c7c4761180bd3891138ca37d8571f52
Stanislav Kozubenko <staspk@gmail.com>
2025-07-22 07:05:31 -0700

    Timer elapsed functionality implemented

--- node/_shared/Timer.ts
+++ node/_shared/Timer.ts
@@ -1,54 +1,54 @@
+import { printRed, printYellow } from "./print.js";
 
 /**
 * Supports both: Browser and Nodejs Environments.
 */
 export class Timer {
-    static YELLOW_ANSI_CODE = '\x1b[93m';
-    static RESET_ANSI_CODE  = '\x1b[0m';
-
     static isBrowser: boolean = null;
-    static startTime: number = null;
-
+    static _startTime: number = null;
+    static _elapsed: number[] = [];
+    
     static start() {
         if(Timer.isBrowser == null)
             Timer.isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
-
-        Timer.startTime = performance.now();
-    }
-    
-    static elapsed(operationName = '') {
-        if (!Timer.startTime)
-            return;
         
-        let elapsed = performance.now() - Timer.startTime;
-            if (elapsed > 1000) {
-                elapsed = elapsed / 1000;
-                Timer.smartPrint(`Operation [${operationName}] timed at: ${elapsed.toFixed(3)}s`);
-            } else
-                Timer.smartPrint(`Operation [${operationName}] timed at: ${elapsed.toFixed(3)}ms`);
+        Timer._startTime = performance.now();
+    }
+
+    /**  Use to time certain operations. Uses `Timer._startTime` on first call, `Timer._elapsed` on every successive call to determine operation time.  */
+    static elapsed(operationName) {
+        if (Timer._startTime == null) {
+            printRed(`Timer.elapsed() called before Timer.start()`);
+            return;
+        }
+
+        const NOW = performance.now();
+
+        let elapsed = Timer._elapsed.length < 1
+            ? NOW - Timer._startTime
+            : NOW - Timer._elapsed[Timer._elapsed.length - 1]
+
+        Timer._elapsed.push(NOW);
+        
+        if (elapsed > 1000) {
+            elapsed = elapsed / 1000;
+            printYellow(`Operation [${operationName}] timed at: ${elapsed.toFixed(3)}s`)
+        } else
+            printYellow(`Operation [${operationName}] timed at: ${elapsed.toFixed(3)}ms`)
     }
     
     static stop() {
-        if (!Timer.startTime)
+        if (Timer._startTime == null)
             return;
         
-        let elapsed = performance.now() - Timer.startTime;
+        let elapsed = performance.now() - Timer._startTime;
         if (elapsed > 1000) {
             elapsed = elapsed / 1000;
-            Timer.smartPrint(`Operation timed at: ${elapsed.toFixed(3)}s`);
+            printYellow(`Timer total: ${elapsed.toFixed(3)}s`)
         } else
-            Timer.smartPrint(`Operation timed at: ${elapsed.toFixed(3)}ms`);
+            printYellow(`Timer total: ${elapsed.toFixed(3)}ms`)
         
-        Timer.startTime = null;
-    }
-    
-    /**
-    * Prints to browser console or process.stdout, depending on environment/runtime.
-    */
-    static smartPrint(text) {
-        if (Timer.isBrowser)
-            console.log(`%c${text}`, 'color: yellow');
-        else
-            process.stdout.write(`${Timer.YELLOW_ANSI_CODE}${text}${Timer.RESET_ANSI_CODE}`);
+        Timer._startTime = null;
+        Timer._elapsed = []
     }
 }
