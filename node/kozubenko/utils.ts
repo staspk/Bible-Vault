785404b7e2c0d0e252fd4142ed2b1a5f269260d9
Stanislav Kozubenko <staspk@gmail.com>
2025-08-26 13:37:00 -0700

    +isNullOrWhitespace()

--- node/kozubenko/utils.ts
+++ node/kozubenko/utils.ts
@@ -1,6 +1,6 @@
 
 /**
- * Combines paths together (handles paths with forward `/`, backslashes `\`, and both)
+ * Combines paths together (handles paths with forward `/`, backslashes `\`, or both)
  *
  * @returns {string} Returns a path with forward slashes `/`
  */
@@ -8,4 +8,13 @@ export function combinePaths(...paths: string[]): string {
     return paths
         .flatMap(path => path.split(/[\\/]/))
         .join('/');
+}
+
+/**
+ * Note: also handles `undefined` (returns: `True`)
+ */
+export function isNullOrWhitespace(str:string): boolean {
+    if(str == null || str.trim() === "")
+        return true;
+    return false;
 }
