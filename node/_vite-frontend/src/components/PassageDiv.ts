5f9e31d9a7a5a4c006da1dd2170e318f19ba4f59
Stanislav Kozubenko <staspk@gmail.com>
2025-09-15 00:34:37 -0700

    >5 total_translations split per 2 views

--- node/_vite-frontend/src/components/PassageDiv.ts
+++ node/_vite-frontend/src/components/PassageDiv.ts
@@ -1,7 +1,5 @@
-import { BIBLE, Book } from "../models/Bible";
-import type { IChapterResponse, IChaptersResponse } from "../../../_shared/interfaces"
-import { printGreen, printYellow } from "../../../_shared/print";
-import { Timer } from "../../../_shared/Timer";
+import { Book } from "../models/Bible";
+import { IResponses, type IChapterResponse } from "../../../_shared/interfaces"
 
 
 /** CSS class that's used to determine `width` and `grid-template-columns` for: `#passage-div` */
@@ -10,15 +8,19 @@ const COLUMNS_CLASS = (total_translations:number) => `cols-${total_translations}
 export class PassageDiv {
     static ID = 'passage-div';
 
-    static mirrorOption = true;    /*  when true: 6,8 translations are halved between 2 tables. */
+    static mirrorOption = true;    /*  when true: 6,8 translations are halved between 2 views. */
+
+    static view1: HTMLDivElement;
+    static view2: HTMLDivElement;
 
     /**  Generates and mounts onto `PassageDiv.PLACEHOLDER` [`#passage-div`]  
      *   Only 1-10 translations per chapter supported  */
     static Generate(book:Book, chapter:number, data:IChapterResponse) {
         const PLACEHOLDER = document.getElementById(PassageDiv.ID);
-        let passageDiv = Object.assign(document.createElement('div'), {
-            id: PLACEHOLDER?.id
-        });
+        if(!PLACEHOLDER) {
+            console.error('PassageDiv.Generate() could not complete because document.getElementById({PassageDiv.ID}) failed...');
+            return;
+        }
 
         let total_translations = 0, view1_translations = 0, view2_translations = 0;
         for (const [i, [key, value]] of Object.entries(data.data).entries())
@@ -35,45 +37,54 @@ export class PassageDiv {
                 view1_translations = total_translations;
         }
 
-        passageDiv.classList.add(COLUMNS_CLASS(view1_translations))
-        passageDiv = PassageDiv.constructPassageView(passageDiv, chapter, view1_translations, data);
-        
-        PLACEHOLDER?.replaceWith(passageDiv);
-        
+        PassageDiv.view1 = PassageDiv.generateView(chapter, view1_translations, IResponses.range(0, view1_translations, data));
+        if(view2_translations > 0)
+            PassageDiv.view2 = PassageDiv.generateView(chapter, view2_translations, IResponses.range(view1_translations-1, (view1_translations+view2_translations), data));
+
+        PLACEHOLDER.replaceWith(PassageDiv.view1);
         PassageDiv.alignVerses();
     }
 
-    static constructPassageView(passageDiv:HTMLDivElement, chapter:number, total_columns:number, data:IChapterResponse): HTMLDivElement {
-        // const columns: Array<HTMLDivElement|null> = [];
+    static generateView(chapter:number, total_translations:number, data:IChapterResponse): HTMLDivElement {
+        const view = Object.assign(document.createElement('div'), {
+            id: PassageDiv.ID
+        });
+
+        view.classList.add(COLUMNS_CLASS(total_translations));
+
         for (const [i, [translation, chapterMap]] of Object.entries(data.data).entries()) {
-            if(i < total_columns) {
-                if (chapterMap == null) continue;   /* If a chap is missing, the translation tables don't match when >5 translations. <=5, don't match COLUMNS_CLASS. Need to design an empty column */
-                
-                const chapterColumn = Object.assign(document.createElement('div'), {
-                    className: 'chapter-column',
-                    id: translation
-                });
-                
-                let row = 1;
-                for (const [verseNumber, verseText] of Object.entries(chapterMap)) {
-                    chapterColumn.append(Object.assign(document.createElement('div'), {
-                        id: `${translation}-${chapter}-${verseNumber}`,
-                        className: `row-${row}`,
-                        innerHTML: verseNumber === '1'
-                        ? `<span class="chapter-dropcap">${chapter}</span> ${verseText}`
-                        : `<span class="versenum">${verseNumber}</span> ${verseText}`
-                    }))
-                    row++;
-                }
-                passageDiv.append(chapterColumn);
+            if (chapterMap == null) continue;   /* If a chap is missing, the translation tables don't match when mirrorOption==true. Need to design an empty column */
+            
+            const chapterColumn = Object.assign(document.createElement('div'), {
+                className: 'chapter-column',
+                id: translation
+            });
+            
+            let row = 1;
+            for (const [verseNumber, verseText] of Object.entries(chapterMap)) {
+                chapterColumn.append(Object.assign(document.createElement('div'), {
+                    id: `${translation}-${chapter}-${verseNumber}`,
+                    className: `row-${row}`,
+                    innerHTML: verseNumber === '1'
+                    ? `<span class="chapter-dropcap">${chapter}</span> ${verseText}`
+                    : `<span class="versenum">${verseNumber}</span> ${verseText}`
+                }))
+                row++;
             }
+            view.append(chapterColumn);
         }
-        return passageDiv;
+        
+        return view;
     }
 
     /**  Helper function to `PassageDiv.Generate()`. Aligns rows so that equal verses between translations start at the same y position.  */
-    static alignVerses() {
-        const PASSAGE_DIV = document.getElementById(PassageDiv.ID) as HTMLDivElement;
+    static alignVerses(div:any=null) {
+        let PASSAGE_DIV = document.getElementById(PassageDiv.ID) as HTMLDivElement;
+
+        if(div) {
+            PASSAGE_DIV = div;
+        }
+        
         let row = 1;
         let nodes = PASSAGE_DIV.querySelectorAll(`.row-${row}`) as NodeListOf<HTMLDivElement>;
         while (nodes.length > 0) {
