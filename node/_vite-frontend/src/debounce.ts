d88f77ee8d714506e38b70dd550f59f7692ac6ef
Stanislav Kozubenko <staspk@gmail.com>
2025-09-16 09:49:53 -0700

    +decounce()

---
+++ node/_vite-frontend/src/debounce.ts
@@ -0,0 +1,7 @@
+export function debounce(fn, delay = 100) {
+    let timeoutId;
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn.apply(this, args), delay);
+    };
+}
