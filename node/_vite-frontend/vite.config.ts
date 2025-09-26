7d5c8f668c786e1a16d920c047d051bff9b82f22
Stanislav Kozubenko <staspk@gmail.com>
2025-09-26 05:15:21 -0700

    multi-page configuration

--- node/_vite-frontend/vite.config.ts
+++ node/_vite-frontend/vite.config.ts
@@ -1,11 +1,22 @@
 // vite.config.js
+import { dirname, resolve } from 'node:path'
+
 export default {
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+         },
       },
-    },
-  },
+   },
+   
+   build: {
+      rollupOptions: {
+         input: {
+            main  : resolve(__dirname, 'index.html'),
+            report: resolve(__dirname, 'report.html')
+         }
+      }
+   }
 };
