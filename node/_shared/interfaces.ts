5ad6daf627bd2553cec4b870bee07b72c9963f7c
Stanislav Kozubenko <staspk@gmail.com>
2025-09-26 21:59:11 -0700

    +IChapter, +IChapters

--- node/_shared/interfaces.ts
+++ node/_shared/interfaces.ts
@@ -3,74 +3,132 @@ import { printGreen, printOrange, printYellow } from "./print.js";
 
 
 /**
-* **Example:**
-* ```json
-* {
-*   "status": "partial",
-*   "data": {
-*     "KJV": null,
-*     "NKJV": {
-*       "1": "This is the book of the genealogy of Adam. In the day that God created man, He made him in the likeness of God.",
-*       "2": "He created them male and female, and blessed them and called them Mankind in the day they were created.",
-*       ...
-*     },
-*     "RSV": {
-*       "1": "This is the book of the generations of Adam. When God created man, he made him in the likeness of God.",
-*       ...
-*     },
-*     "NRSV": {
-*       "1": "This is the list of the descendants of Adam. When God created humans, he made them in the likeness of God.",
-*       ...
-*     },
-*     "NASB": {
-*       "1": "This is the book of the generations of Adam. On the day when God created man, He made him in the likeness of God.",
-*       ...
-*     }
-*   }
-* }
-* ```
+* ***EXAMPLE:***
+```json
+{
+    "KJV": null,
+    "NKJV": {
+      "1": "Now it came to pass, when Isaac was old and his eyes were so dim that he could not see, that he called Esau.",
+      "2": "Then he said, “Behold now, I am old. I do not know the day of my death.",
+      ...
+    },
+    "RSV": {
+      "1": "This is the book of the generations of Adam. When God created man, he made him in the likeness of God.",
+      "2": "Male and female he created them, and he blessed them and named them Man when they were created.",
+      ...
+    },
+    ...
+}
+```
 */
-export interface IChapterResponse {
-    status: `${Status}`;
-    data: {
-        [translation: string]: { [verseNumber: string]: string } | null;
+export interface IChapter {
+    [translation: string]: { [verseNumber: string]: string } | null;
+}
+
+/**
+* ***EXAMPLE:***
+```json
+{
+    "27": {
+      "KJV": null,
+      "NKJV": {
+        "1": "Now it came to pass, when Isaac was old and his eyes were so dim that he could not see, that he called Esau.",
+        "2": "Then he said, “Behold now, I am old. I do not know the day of my death.",
+        ...
+      }
+    },
+    "28": {
+      "KJV": {
+        "1": "And Isaac called Jacob, and blessed him, and charged him, and said unto him, Thou shalt not take a wife...",
+        "2": "Arise, go to Padanaram, to the house of Bethuel thy mother's father; and take thee a wife from thence...",
+        ...
+      },
+      "NKJV": {
+        "1": "Then Isaac called Jacob and blessed him, and [a]charged him, and said to him: “You shall not take a wife...",
+        "2": "Arise, go to Padan Aram, to the house of Bethuel your mother’s father; and take yourself a wife...",
+        ...
+      }
+    }
+}
+```
+*/
+export interface IChapters {
+    [chapter: string]: {
+        [translation: string]: {
+            [verseNumber: string]: string
+        } | null;
     }
 }
 
 /**
-* **Example:**
-* ```json
-* {
-*   "status": "partial",
-*   "data": {
-*      "27": {
-*         "KJV": null,
-*         "NKJV": {
-*            "1": Now it came to pass, when Isaac was old and his eyes were so dim that he could not see, that he called Esau his older son and said to him, "My son." And he answered him, "Here I am.",
-*            "2": "Then he said, “Behold now, I am old. I do not know the day of my death.",
-*            ...
-*      },
-*      "28": {
-*         "KJV": {
-*            "1": "And Isaac called Jacob, and blessed him, and charged him, and said unto him, Thou shalt not take a wife of the daughters of Canaan.",
-*            "2": "Arise, go to Padanaram, to the house of Bethuel thy mother's father; and take thee a wife from thence of the daughers of Laban thy mother's brother."
-*         }
-*         "NKJV": {
-*            "1": "Then Isaac called Jacob and blessed him, and [a]charged him, and said to him: “You shall not take a wife from the daughters of Canaan.",
-*            "2": "Arise, go to Padan Aram, to the house of Bethuel your mother’s father; and take yourself a wife from there of the daughters of Laban your mother’s brother.",
-*            ...
-*          }
-*      }
-* }
-* ```
+* ***EXAMPLE:***
+```json
+{
+    "status": "success",
+    "data": {
+      "KJV": null,
+      "NKJV": {
+        "1": "This is the book of the genealogy of Adam. In the day that God created man, He made him in the likeness of God.",
+        "2": "He created them male and female, and blessed them and called them Mankind in the day they were created.",
+        ...
+      },
+      "RSV": {
+        "1": "This is the book of the generations of Adam. When God created man, he made him in the likeness of God.",
+        "2": "Male and female he created them, and he blessed them and named them Man when they were created.",
+        ...
+      },
+      "NRSV": {
+        "1": "This is the list of the descendants of Adam. When God created humans, he made them in the likeness of God.",
+        "2": "Male and female he created them, and he blessed them and called them humans when they were created.",
+        ...
+      },
+      "NASB": {
+        "1": "This is the book of the generations of Adam. On the day when God created man, He made him in the likeness of God.",
+        "2": "He created them male and female, and He blessed them and named them “mankind” on the day when they were created."
+        ...
+      }
+    }
+}
+```
+*/
+export interface IChapterResponse {
+    status: `${Status}`;
+    data: IChapter
+}
+
+/**
+* ***EXAMPLE:***
+```json
+{
+    "status": "partial",
+    "data": {
+      "27": {
+        "KJV": null,
+        "NKJV": {
+          "1": "Now it came to pass, when Isaac was old and his eyes were so dim that he could not see, that he called Esau...",
+          "2": "Then he said, “Behold now, I am old. I do not know the day of my death.",
+          ...
+        }
+      },
+      "28": {
+        "KJV": {
+          "1": "And Isaac called Jacob, and blessed him, and charged him, and said unto him, Thou shalt not take a wife...",
+          "2": "Arise, go to Padanaram, to the house of Bethuel thy mother's father; and take thee a wife from thence...",
+          ...
+        },
+        "NKJV": {
+          "1": "Then Isaac called Jacob and blessed him, and [a]charged him, and said to him: “You shall not take a wife...",
+          "2": "Arise, go to Padan Aram, to the house of Bethuel your mother’s father; and take yourself a wife...",
+          ...
+        }
+      }
+    }
+}
+```
 */
 export interface IChaptersResponse {
     status: `${Status}`;
-    data: {
-        [chapterNumber: string]: {
-            [translation: string]: { [verseNumber: string]: string } | null;
-        }
-    }
+    data: IChapters
 }
 
 export class IResponses {
