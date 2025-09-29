54bf5afb885b9c37e9d435d5a01b17bda24da9e3
Stanislav Kozubenko <staspk@gmail.com>
2025-09-28 23:21:44 -0700

    +IResponses.wrapAsResponse()

--- node/_shared/interfaces.ts
+++ node/_shared/interfaces.ts
@@ -178,4 +178,29 @@ export class IResponses {
             }
         }
     }
+
+    static wrapAsResponse(chapter:IChapter): IChapterResponse {
+        let amountNull = 0, total = 0;
+        for (const [translation, verses] of Object.entries(chapter))
+            if(verses === null)
+                amountNull++;
+            total++;
+
+        if (amountNull === 0) {
+            return {
+                status: Status.Success,
+                data: chapter
+            }
+        } else if(total === amountNull) {
+            return {
+                status: Status.NotFound,
+                data: chapter
+            }
+        } else {
+            return {
+                status: Status.Partial,
+                data: chapter
+            }
+        }
+    }
 }
