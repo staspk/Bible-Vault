d88fe75f720354e2f6c69e3470784ec2e417fe60
Stanislav Kozubenko <staspk@gmail.com>
2025-11-20 08:58:14 -0800

    api.ts =(renamed)> BibleApi.ts

--- node/_vite-frontend/src/services/BibleApi.ts
+++ node/_vite-frontend/src/services/BibleApi.ts
@@ -1,5 +1,5 @@
-import type { Book } from "../models/Bible";
-import type { BibleSearch } from "../models/BibleSearch";
+import type { Book } from "../models/Bible.js";
+import type { BibleSearch } from "../models/BibleSearch.js";
 
 /** `/api/bible` */
 export class BibleApi {
