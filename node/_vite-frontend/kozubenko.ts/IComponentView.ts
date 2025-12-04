38f8aaeaf6884b709e0b9dafa6036171a3a116e2
Stanislav Kozubenko <staspk@gmail.com>
2025-12-03 17:45:13 -0800

    ReplaceWith() now part of IViewComponent

--- node/_vite-frontend/kozubenko.ts/IComponentView.ts
+++ node/_vite-frontend/kozubenko.ts/IComponentView.ts
@@ -1,3 +1,7 @@
+import { isNullOrUndefined } from "../../kozubenko/utils.js";
+import { Document } from "./Document.js";
+
+
 /** static interface */
 export interface IViewComponent {
     ID:string;
@@ -5,4 +9,25 @@ export interface IViewComponent {
 
     /** (built-in) class name */
     name:string;
+}
+
+export class IViewComponent {
+
+    /** First Step of an IViewComponent `Render()`:
+    - `isNullOrUndefined(onto)` Assertion - `console.error` report, If: `true`
+    - `component.ID` determines `id` of new Div that replaces `onto` in the DOM.
+    - `component.Element` becomes ptr to the new Div
+     */
+    static ReplaceWith(onto:HTMLElement, component:IViewComponent): false | IViewComponent {
+        if(isNullOrUndefined(onto)) {
+            console.error(`${component.name}.Render(): onto is null/undefined. Cannot complete Render...`);
+            return false;
+        }
+        const componentDiv = Document('div', {
+            id: component.ID
+        }); onto.replaceWith(componentDiv);
+
+        component.Element = componentDiv;
+        return component;
+    }
 }
