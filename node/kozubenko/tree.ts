ed373e3d718c3a19a9cf2d5c6cb5e73b2ba2ca46
Stanislav Kozubenko <staspk@gmail.com>
2025-12-05 15:00:24 -0800

    og version

---
+++ node/kozubenko/tree.ts
@@ -0,0 +1,42 @@
+/** Assumes Node represents a step in a search string path */
+export class TreeNode {
+    constructor(
+        public char:string,
+        public depth = 0,
+        public children:TreeNode[]|null = null
+    ){}
+}
+
+export class Tree {
+    static From(list:string[]): TreeNode {
+        const root = new TreeNode(list[0][0]);              /* Assumes first character of every string is the same */
+
+        let node = root;
+        list.forEach(word => {
+            for (let i = 0; i < word.length; i++) {
+                let char = word[i+1];
+                if(char) {
+
+                    node = Tree.walk(root, word.slice(0, i+2));
+
+                    let child = new TreeNode(char, i);
+                    if(node.children)
+                        node.children.push(child);
+                    else node.children = [child];
+                }  
+            }
+            node = root;
+        });
+
+        return root;
+    }
+
+    /** Assumes: caller guarantees that root has a path using str */
+    static walk(root:TreeNode, str:string): TreeNode {
+        let node = root;
+        for (const char of str)
+            node = node.children?.find(node => node.char === char) as TreeNode;
+
+        return node;
+    }
+}
