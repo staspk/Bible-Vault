6addf2aa7d7d0af66c8deb019eb4a07de2b982a9
Stanislav Kozubenko <staspk@gmail.com>
2026-01-08 23:49:43 -0800

    deleted unecessary

--- python/definitions.py
+++ python/definitions.py
@@ -7,20 +7,11 @@ DEFINITIONS_PY                =  os.path.abspath(__file__)
 PYTHON_ROOT_DIRECTORY         =  Parent(DEFINITIONS_PY)
 PROJECT_ROOT_DIRECTORY        =  Parent(PYTHON_ROOT_DIRECTORY)
 
-REPORTS_DIRECTORY             =  os.path.join(PYTHON_ROOT_DIRECTORY, 'reports')
-
-TEMP_DIRECTORY                =  os.path.join(PROJECT_ROOT_DIRECTORY, 'temp')
-JSONS_DIRECTORY               =  os.path.join(PROJECT_ROOT_DIRECTORY, 'jsons')
-
-BIBLE_JSON                    =  os.path.join(JSONS_DIRECTORY, 'bible.json')
-
-TEMPORARY_HTML                =  os.path.join(PYTHON_ROOT_DIRECTORY, 'temporary.html')
+PYTHON_TESTS_DIRECTORY        =  os.path.join(PYTHON_ROOT_DIRECTORY, 'tests')
 
 BIBLE_HTML                    =  os.path.join(PROJECT_ROOT_DIRECTORY, 'bible_html')
 BIBLE_TXT                     =  os.path.join(PROJECT_ROOT_DIRECTORY, 'bible_txt')
 BIBLE_TXT_NEW                 =  os.path.join(PYTHON_ROOT_DIRECTORY, 'bible_txt')
-
-BIBLE_NUMERICAL_MAP           =  os.path.join(PROJECT_ROOT_DIRECTORY, 'bible_numerical_map')
 
 TEMP_OUTPUT                   =  os.path.join(PYTHON_ROOT_DIRECTORY, 'output.txt')
 
